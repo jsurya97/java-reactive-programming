91929aab2b14362874473ab0c7e526f7587ba715
Jayasurya <jsuryagowri@gmail.com>
2025-08-02 20:14:16 +0530

    Hot cold publisher

---
+++ 01-reactive-programming-playground/src/main/java/com/jk/sec06/Lec02HotPublisher.java
@@ -0,0 +1,57 @@
+package com.jk.sec06;
+
+import com.jk.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/*
+    Hot - 1 data producer for all the subscribers.
+    share => publish().refCount(1)
+    It needs 1 min subscriber to emit data.
+    It stops when there is 0 subscriber.
+    Re-subscription - It starts again where there is a new subscriber.
+    To have min 2 subscribers, use publish().refCount(2);
+ */
+public class Lec02HotPublisher {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec02HotPublisher.class);
+
+    public static void main(String[] args) {
+        var movieFlux = moviewStream().share();
+        Util.sleepSeconds(2);
+
+        movieFlux
+                .take(4)
+                .subscribe(Util.subscriber("same"));
+
+        Util.sleepSeconds(3);
+
+        movieFlux
+                .take(3)
+                .subscribe(Util.subscriber("mike"));
+
+
+        Util.sleepSeconds(15);
+
+    }
+
+    private static Flux<String> moviewStream() {
+        return Flux.generate(() -> {
+                            log.info("recived the req");
+                            return 1;
+                        },
+                        (state, sink) -> {
+                            var scene = "moviee" + state;
+                            log.info("playing {}", scene);
+                            sink.next(scene);
+                            return ++state;
+
+                        })
+                .take(10)
+                .delayElements(Duration.ofSeconds(1))
+                .cast(String.class);
+    }
+}
